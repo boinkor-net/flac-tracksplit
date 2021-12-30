38ae6eec70d6a6746fc3429809dac0332785d987
Andreas Fuchs <asf@boinkor.net>
2021-12-30 20:33:57 +0100

    Add filename-generation

--- flac-tracksplit/examples/split_file.rs
+++ flac-tracksplit/examples/split_file.rs
@@ -1,10 +1,10 @@
-use std::fs::File;
+use std::{fs::File, path::PathBuf, str::FromStr};
 
 use symphonia_bundle_flac::FlacReader;
 use symphonia_core::{
     formats::{Cue, FormatReader},
     io::MediaSourceStream,
-    meta::Tag,
+    meta::{Tag, Value, Visual},
 };
 use symphonia_utils_xiph::flac::metadata::StreamInfo;
 
@@ -26,7 +26,9 @@ fn main() {
     let cues: Vec<Cue> = reader.cues().iter().cloned().collect();
     println!("cues: {:?}", cues);
     let metadata = reader.metadata();
-    let tags = metadata.current().expect("tags").tags();
+    let current_metadata = metadata.current().expect("tags");
+    let tags = current_metadata.tags();
+    let visuals = current_metadata.visuals();
 
     let mut cue_iter = cues.iter().peekable();
     while let Some(cue) = cue_iter.next() {
@@ -35,8 +37,13 @@ fn main() {
             // we have a lead-out, capture the whole in the last track.
             next = None;
         }
-        let track = Track::from_tags(cue, next, &tags);
-        println!("\ntrack {}: {:?}", track.number, track);
+        let track = Track::from_tags(cue, next, &tags, &visuals);
+        println!(
+            "\ntrack {}: {:?} = {:?}",
+            track.number,
+            track.filename(),
+            track
+        );
         if next.is_none() {
             break;
         }
@@ -45,12 +52,24 @@ fn main() {
 
 const LEAD_OUT_TRACK_NUMBER: u32 = 170;
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 struct Track {
     number: u32,
     start_ts: u64,
     end_ts: Option<u64>,
     tags: Vec<Tag>,
+    visuals: Vec<Visual>,
+}
+
+impl std::fmt::Debug for Track {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Track")
+            .field("number", &self.number)
+            .field("start_ts", &self.start_ts)
+            .field("end_ts", &self.end_ts)
+            .field("tags", &self.tags)
+            .finish()
+    }
 }
 
 impl Track {
@@ -58,7 +77,7 @@ impl Track {
         !name.ends_with("]") && name != "CUESHEET" && name != "LOG"
     }
 
-    fn from_tags(cue: &Cue, next: Option<&&Cue>, tags: &[Tag]) -> Self {
+    fn from_tags(cue: &Cue, next: Option<&&Cue>, tags: &[Tag], visuals: &[Visual]) -> Self {
         let suffix = format!("[{}]", cue.index);
         let tags = tags
             .into_iter()
@@ -73,11 +92,48 @@ impl Track {
                 tag_name.map(|key| Tag::new(tag.std_key, key, tag.value.clone()))
             })
             .collect();
+        let visuals = visuals.into_iter().cloned().collect();
         Self {
             number: cue.index,
             start_ts: cue.start_ts,
             end_ts: next.map(|cue| cue.start_ts),
             tags,
+            visuals,
         }
+    }
+
+    fn tag_value(&self, name: &str) -> Option<&Value> {
+        self.tags
+            .iter()
+            .find(|tag| tag.key == name)
+            .map(|found| &found.value)
+    }
+
+    fn filename(&self) -> PathBuf {
+        let mut buf = PathBuf::new();
+        if let Some(Value::String(artist)) = self.tag_value("ALBUMARTIST") {
+            buf.push(artist);
+        } else if let Some(Value::String(artist)) = self.tag_value("ARTIST") {
+            buf.push(artist);
+        } else {
+            buf.push("Unknown Artist");
+        }
+
+        if let Some(Value::String(album)) = self.tag_value("ALBUM") {
+            buf.push(album);
+        } else {
+            buf.push("Unknown Album");
+        }
+
+        if let (Some(Value::String(track)), Some(Value::String(title))) =
+            (self.tag_value("TRACKNUMBER"), self.tag_value("TITLE"))
+        {
+            if let Ok(trackno) = <usize as FromStr>::from_str(track) {
+                buf.push(format!("{:02}.{}.flac", trackno, title));
+            } else {
+                buf.push(format!("99.{}.flac", title));
+            }
+        }
+        buf
     }
 }
