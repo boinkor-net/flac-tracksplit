4f4aafb3b0b9a9bb54be3d9f1559a4f8e9b31746
Andreas Fuchs <asf@boinkor.net>
2022-01-01 12:48:48 +0100

    Write actual samples
    They play back but VOX has problems showing their length. Also, the
    last track panics. Weird.

--- flac-tracksplit/examples/split_file.rs
+++ flac-tracksplit/examples/split_file.rs
@@ -1,14 +1,15 @@
-use std::{
-    fs::{create_dir_all, File},
-    io::{self, Write},
-    num::NonZeroU32,
-    path::PathBuf,
-    str::FromStr,
-};
-
+use anyhow::Context;
 use metaflac::{
     block::{Picture, PictureType, StreamInfo, VorbisComment},
     Block,
+};
+use std::{
+    borrow::Cow,
+    fs::{create_dir_all, File},
+    io::Write,
+    num::NonZeroU32,
+    path::{is_separator, PathBuf},
+    str::FromStr,
 };
 use symphonia_bundle_flac::FlacReader;
 use symphonia_core::{
@@ -22,43 +23,60 @@ fn main() {
     let mss = MediaSourceStream::new(Box::new(file), Default::default());
     let mut reader = FlacReader::try_new(mss, &Default::default()).expect("creating flac reader");
     // println!("tracks: {:?}", reader.tracks());
-    if let Some(data) = &reader
-        .default_track()
-        .expect("default track")
-        .codec_params
-        .extra_data
-    {
-        let info = StreamInfo::from_bytes(data);
-        let cues: Vec<Cue> = reader.cues().iter().cloned().collect();
-        let metadata = reader.metadata();
-        let current_metadata = metadata.current().expect("tags");
-        let tags = current_metadata.tags();
-        let visuals = current_metadata.visuals();
+    let track = reader.default_track().expect("default track");
+    let data = match &track.codec_params.extra_data {
+        Some(it) => it,
+        _ => return,
+    };
+    let info = StreamInfo::from_bytes(data);
+    let cues: Vec<Cue> = reader.cues().iter().cloned().collect();
+    let time_base = track.codec_params.time_base.expect("time base");
+    assert_eq!(time_base.numer, 1, "Should be a fraction like 1/44000");
+    assert_eq!(
+        time_base.denom, info.sample_rate,
+        "Should have the sample rate as denom"
+    );
+    // since we're sure that the sample rate is an even denominator of
+    // symphonia's TimeBase, we can assume that the time stamps are in
+    // samples:
+    let last_ts: u64 = info.total_samples;
 
-        let mut cue_iter = cues.iter().peekable();
-        while let Some(cue) = cue_iter.next() {
-            let mut next = cue_iter.peek();
-            if let Some(LEAD_OUT_TRACK_NUMBER) = next.map(|n| n.index) {
+    let mut cue_iter = cues.iter().peekable();
+    while let Some(cue) = cue_iter.next() {
+        let next = cue_iter.peek();
+        let end_ts = match next {
+            None => last_ts,
+            Some(track) if track.index == LEAD_OUT_TRACK_NUMBER => {
                 // we have a lead-out, capture the whole in the last track.
-                next = None;
                 cue_iter.next();
+                last_ts
             }
-            let track = Track::from_tags(&info, cue, next, &tags, &visuals);
-            println!(
-                "\ntrack {}: {:?} = {:?}",
-                track.number,
-                track.pathname(),
-                track
-            );
-            let path = track.pathname();
-            if let Some(parent) = path.parent() {
-                create_dir_all(parent).expect("creating album dir");
-            }
-            let f = File::create(track.pathname()).unwrap();
+            Some(track) => track.start_ts,
+        };
+        let track = {
+            let metadata = reader.metadata();
+            let current_metadata = metadata.current().expect("tags");
+            let tags = current_metadata.tags();
+            let visuals = current_metadata.visuals();
+            Track::from_tags(&info, cue, end_ts, &tags, &visuals)
+        };
+        println!(
+            "\ntrack {}: {:?} = {:?}",
+            track.number,
+            track.pathname(),
             track
-                .write_out(f)
-                .expect(&format!("writing track {:?}", track.pathname()));
+        );
+        let path = track.pathname();
+        if let Some(parent) = path.parent() {
+            create_dir_all(parent).expect("creating album dir");
         }
+        let mut f = File::create(track.pathname()).unwrap();
+        track
+            .write_metadata(&mut f)
+            .expect(&format!("writing track {:?}", track.pathname()));
+        track
+            .write_audio(&mut reader, &mut f)
+            .expect("writing track");
     }
 }
 
@@ -69,7 +87,7 @@ struct Track {
     streaminfo: StreamInfo,
     number: u32,
     start_ts: u64,
-    end_ts: Option<u64>,
+    end_ts: u64,
     tags: Vec<Tag>,
     visuals: Vec<Visual>,
 }
@@ -93,7 +111,7 @@ impl Track {
     fn from_tags(
         streaminfo: &StreamInfo,
         cue: &Cue,
-        next: Option<&&Cue>,
+        end_ts: u64,
         tags: &[Tag],
         visuals: &[Visual],
     ) -> Self {
@@ -115,12 +133,12 @@ impl Track {
         Self {
             streaminfo: StreamInfo {
                 md5: [0u8; 16].to_vec(),
-                total_samples: 0, // TODO: maybe figure out how long each track is
+                total_samples: 0, // (end_ts - cue.start_ts)
                 ..streaminfo.clone()
             },
             number: cue.index,
             start_ts: cue.start_ts,
-            end_ts: next.map(|cue| cue.start_ts),
+            end_ts,
             tags,
             visuals,
         }
@@ -133,21 +151,29 @@ impl Track {
             .map(|found| &found.value)
     }
 
+    fn sanitize_pathname(name: &str) -> Cow<str> {
+        if name.contains(is_separator) {
+            Cow::Owned(name.replace(is_separator, "_"))
+        } else {
+            Cow::Borrowed(name)
+        }
+    }
+
     fn pathname(&self) -> PathBuf {
         let mut buf = PathBuf::new();
         if let Some(Value::String(artist)) = self.tag_value("ALBUMARTIST") {
-            buf.push(artist);
+            buf.push(Self::sanitize_pathname(artist).as_ref());
         } else if let Some(Value::String(artist)) = self.tag_value("ARTIST") {
-            buf.push(artist);
+            buf.push(Self::sanitize_pathname(artist).as_ref());
         } else {
             buf.push("Unknown Artist");
         }
 
         if let Some(Value::String(album)) = self.tag_value("ALBUM") {
             if let Some(Value::String(year)) = self.tag_value("DATE") {
-                buf.push(format!("{} - {}", year, album));
+                buf.push(format!("{} - {}", year, Self::sanitize_pathname(album)));
             } else {
-                buf.push(album);
+                buf.push(Self::sanitize_pathname(album).as_ref());
             }
         } else {
             buf.push("Unknown Album");
@@ -157,15 +183,19 @@ impl Track {
             (self.tag_value("TRACKNUMBER"), self.tag_value("TITLE"))
         {
             if let Ok(trackno) = <usize as FromStr>::from_str(track) {
-                buf.push(format!("{:02}.{}.flac", trackno, title));
+                buf.push(format!(
+                    "{:02}.{}.flac",
+                    trackno,
+                    Self::sanitize_pathname(title)
+                ));
             } else {
-                buf.push(format!("99.{}.flac", title));
+                buf.push(format!("99.{}.flac", Self::sanitize_pathname(title)));
             }
         }
         buf
     }
 
-    fn write_out<S: Write>(&self, mut to: S) -> anyhow::Result<()> {
+    fn write_metadata<S: Write>(&self, mut to: S) -> anyhow::Result<()> {
         to.write_all(b"fLaC")?;
         let comment = VorbisComment {
             vendor_string: "asf's silly track splitter".to_string(),
@@ -204,5 +234,28 @@ impl Track {
             block.write_to(blocks.peek().is_none(), &mut to)?;
         }
         Ok(())
+    }
+
+    fn write_audio<S: Write>(&self, from: &mut FlacReader, mut to: S) -> anyhow::Result<()> {
+        // TODO: Maybe seek. Currently, this is only called in sequence, so no need to do that.
+        let mut last_end: u64 = 0;
+        loop {
+            // TODO: the "end" logic below is wrong.
+            let packet = from
+                .next_packet()
+                .with_context(|| format!("last end: {:?} vs {:?}", last_end, self.end_ts))?;
+            let ts = packet.pts();
+            assert!(
+                ts >= self.start_ts,
+                "Packet timestamp {:?} is not >= this track's start ts {:?}",
+                ts,
+                self.start_ts
+            );
+            to.write_all(packet.buf())?;
+            last_end = ts + packet.duration();
+            if ts + packet.duration() >= self.end_ts {
+                return Ok(());
+            }
+        }
     }
 }
