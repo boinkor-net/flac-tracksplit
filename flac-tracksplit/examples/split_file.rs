e2729afa60b7df3ca49236510af4296643fc5610
Andreas Fuchs <asf@boinkor.net>
2023-05-16 16:39:12 -0400

    Extract the "main" function body and make it parallelizable
    Next up, let's go and use clap to make a real main function.

--- flac-tracksplit/examples/split_file.rs
+++ flac-tracksplit/examples/split_file.rs
@@ -1,13 +1,4 @@
-use std::fs::{create_dir_all, File};
-
-use flac_tracksplit::{Track, LEAD_OUT_TRACK_NUMBER};
-use metaflac::block::StreamInfo;
-use symphonia_bundle_flac::FlacReader;
-use symphonia_core::{
-    formats::{Cue, FormatReader},
-    io::MediaSourceStream,
-};
-use tracing::{debug, info};
+use flac_tracksplit::split_one_file;
 use tracing_subscriber::prelude::*;
 use tracing_subscriber::EnvFilter;
 
@@ -27,60 +18,5 @@ fn main() {
         .with(app_log_layer)
         .with(indicatif_layer)
         .init();
-
-    let file = File::open("test.flac").expect("opening test.flac");
-    let mss = MediaSourceStream::new(Box::new(file), Default::default());
-    let mut reader = FlacReader::try_new(mss, &Default::default()).expect("creating flac reader");
-    debug!("tracks: {:?}", reader.tracks());
-    let track = reader.default_track().expect("default track");
-    let data = match &track.codec_params.extra_data {
-        Some(it) => it,
-        _ => return,
-    };
-    let info = StreamInfo::from_bytes(data);
-    let cues: Vec<Cue> = reader.cues().iter().cloned().collect();
-    let time_base = track.codec_params.time_base.expect("time base");
-    assert_eq!(time_base.numer, 1, "Should be a fraction like 1/44000");
-    assert_eq!(
-        time_base.denom, info.sample_rate,
-        "Should have the sample rate as denom"
-    );
-    // since we're sure that the sample rate is an even denominator of
-    // symphonia's TimeBase, we can assume that the time stamps are in
-    // samples:
-    let last_ts: u64 = info.total_samples;
-
-    let mut cue_iter = cues.iter().peekable();
-    while let Some(cue) = cue_iter.next() {
-        let next = cue_iter.peek();
-        let end_ts = match next {
-            None => last_ts, // no lead-out, fudge it.
-            Some(track) if track.index == LEAD_OUT_TRACK_NUMBER => {
-                // we have a lead-out, capture the whole in the last track.
-                let end_ts = track.start_ts;
-                cue_iter.next();
-                end_ts
-            }
-            Some(track) => track.start_ts,
-        };
-        let track = {
-            let metadata = reader.metadata();
-            let current_metadata = metadata.current().expect("tags");
-            let tags = current_metadata.tags();
-            let visuals = current_metadata.visuals();
-            Track::from_tags(&info, cue, end_ts, &tags, &visuals)
-        };
-        info!(number = track.number, pathname = ?track.pathname(), start_ts = track.start_ts, end_ts = track.end_ts);
-        let path = track.pathname();
-        if let Some(parent) = path.parent() {
-            create_dir_all(parent).expect("creating album dir");
-        }
-        let mut f = File::create(track.pathname()).unwrap();
-        track
-            .write_metadata(&mut f)
-            .expect(&format!("writing track {:?}", track.pathname()));
-        track
-            .write_audio(&mut reader, &mut f)
-            .expect("writing track");
-    }
+    split_one_file("test.flac").expect("Correctly split");
 }
--- flac-tracksplit/src/lib.rs
+++ flac-tracksplit/src/lib.rs
@@ -1,4 +1,4 @@
-use anyhow::Context;
+use anyhow::{bail, Context};
 use int_conv::Truncate;
 use metaflac::{
     block::{Picture, PictureType, StreamInfo, VorbisComment},
@@ -7,19 +7,82 @@ use metaflac::{
 use more_asserts as ma;
 use std::{
     borrow::Cow,
+    fs::{create_dir_all, File},
     io::Write,
     num::NonZeroU32,
-    path::{is_separator, PathBuf},
+    path::{is_separator, Path, PathBuf},
     str::FromStr,
 };
 use symphonia_bundle_flac::FlacReader;
 use symphonia_core::{
     checksum::{Crc16Ansi, Crc8Ccitt},
     formats::{Cue, FormatReader, Packet},
-    io::{Monitor, ReadBytes},
+    io::{MediaSourceStream, Monitor, ReadBytes},
     meta::{Tag, Value, Visual},
 };
-use tracing::{debug, instrument};
+use tracing::{debug, info, instrument};
+
+pub fn split_one_file<P: AsRef<Path>>(input_path: P) -> anyhow::Result<Vec<PathBuf>> {
+    let file = File::open(input_path).expect("opening test.flac");
+    let mss = MediaSourceStream::new(Box::new(file), Default::default());
+    let mut reader = FlacReader::try_new(mss, &Default::default()).expect("creating flac reader");
+    debug!("tracks: {:?}", reader.tracks());
+    let track = reader.default_track().expect("default track");
+    let data = match &track.codec_params.extra_data {
+        Some(it) => it,
+        _ => bail!("Unclear track codec params - Not a flac file?"),
+    };
+    let info = StreamInfo::from_bytes(data);
+    let cues: Vec<Cue> = reader.cues().iter().cloned().collect();
+    let time_base = track.codec_params.time_base.context("track time base")?;
+    assert_eq!(time_base.numer, 1, "Should be a fraction like 1/44000");
+    assert_eq!(
+        time_base.denom, info.sample_rate,
+        "Should have the sample rate as denom"
+    );
+    // since we're sure that the sample rate is an even denominator of
+    // symphonia's TimeBase, we can assume that the time stamps are in
+    // samples:
+    let last_ts: u64 = info.total_samples;
+
+    let mut track_paths = vec![];
+    let mut cue_iter = cues.iter().peekable();
+    while let Some(cue) = cue_iter.next() {
+        let next = cue_iter.peek();
+        let end_ts = match next {
+            None => last_ts, // no lead-out, fudge it.
+            Some(track) if track.index == LEAD_OUT_TRACK_NUMBER => {
+                // we have a lead-out, capture the whole in the last track.
+                let end_ts = track.start_ts;
+                cue_iter.next();
+                end_ts
+            }
+            Some(track) => track.start_ts,
+        };
+        let track = {
+            let metadata = reader.metadata();
+            let current_metadata = metadata.current().context("track tags")?;
+            let tags = current_metadata.tags();
+            let visuals = current_metadata.visuals();
+            Track::from_tags(&info, cue, end_ts, &tags, &visuals)
+        };
+        info!(number = track.number, pathname = ?track.pathname(), start_ts = track.start_ts, end_ts = track.end_ts);
+        let pathbuf = track.pathname();
+        let path = &pathbuf;
+        if let Some(parent) = path.parent() {
+            create_dir_all(parent).context("creating album dir")?;
+        }
+        let mut f = File::create(path).unwrap();
+        track
+            .write_metadata(&mut f)
+            .with_context(|| format!("writing track {:?}", path))?;
+        track
+            .write_audio(&mut reader, &mut f)
+            .with_context(|| format!("writing track {:?} audio", path))?;
+        track_paths.push(pathbuf);
+    }
+    Ok(track_paths)
+}
 
 /// The track number used to identify a lead-out track on a cue sheet.
 pub const LEAD_OUT_TRACK_NUMBER: u32 = 170;
