a870823945b4cc659431fef17872efd517cac21c
Andreas Fuchs <asf@boinkor.net>
2023-05-16 12:35:31 -0400

    Extract the Track struct to the library

--- flac-tracksplit/src/lib.rs
+++ flac-tracksplit/src/lib.rs
@@ -1,14 +1,238 @@
-use std::io::Write;
-
 use anyhow::Context;
 use int_conv::Truncate;
+use metaflac::{
+    block::{Picture, PictureType, StreamInfo, VorbisComment},
+    Block,
+};
+use more_asserts as ma;
+use std::{
+    borrow::Cow,
+    io::Write,
+    num::NonZeroU32,
+    path::{is_separator, PathBuf},
+    str::FromStr,
+};
+use symphonia_bundle_flac::FlacReader;
 use symphonia_core::{
     checksum::{Crc16Ansi, Crc8Ccitt},
-    formats::Packet,
+    formats::{Cue, FormatReader, Packet},
     io::{Monitor, ReadBytes},
+    meta::{Tag, Value, Visual},
 };
-use tracing::debug;
+use tracing::{debug, instrument};
 
+/// The track number used to identify a lead-out track on a cue sheet.
+pub const LEAD_OUT_TRACK_NUMBER: u32 = 170;
+
+/// Metadata identifying a track in a FLAC file that has an embedded CUE sheet.
+#[derive(Clone)]
+pub struct Track {
+    streaminfo: StreamInfo,
+    pub number: u32,
+    pub start_ts: u64,
+    pub end_ts: u64,
+    pub tags: Vec<Tag>,
+    pub visuals: Vec<Visual>,
+}
+
+impl std::fmt::Debug for Track {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("Track")
+            .field("number", &self.number)
+            .field("start_ts", &self.start_ts)
+            .field("end_ts", &self.end_ts)
+            .field("tags", &self.tags)
+            .finish()
+    }
+}
+
+impl Track {
+    fn interesting_tag(name: &str) -> bool {
+        !name.ends_with("]") && name != "CUESHEET" && name != "LOG"
+    }
+
+    /// Create a [Track] from a file's embedded FLAC&vorbis comments and CUE sheet.
+    pub fn from_tags(
+        streaminfo: &StreamInfo,
+        cue: &Cue,
+        end_ts: u64,
+        tags: &[Tag],
+        visuals: &[Visual],
+    ) -> Self {
+        let suffix = format!("[{}]", cue.index);
+        let tags = tags
+            .into_iter()
+            .filter_map(|tag| {
+                let tag_name = if tag.key.ends_with(&suffix) {
+                    Some(&tag.key[0..(tag.key.len() - suffix.len())])
+                } else if Self::interesting_tag(&tag.key) {
+                    Some(tag.key.as_str())
+                } else {
+                    None
+                };
+                tag_name.map(|key| Tag::new(tag.std_key, key, tag.value.clone()))
+            })
+            .collect();
+        let visuals = visuals.into_iter().cloned().collect();
+        Self {
+            streaminfo: StreamInfo {
+                md5: [0u8; 16].to_vec(),
+                total_samples: (end_ts - cue.start_ts),
+                ..streaminfo.clone()
+            },
+            number: cue.index,
+            start_ts: cue.start_ts,
+            end_ts,
+            tags,
+            visuals,
+        }
+    }
+
+    /// Return the tag value for a given tag name.
+    pub fn tag_value(&self, name: &str) -> Option<&Value> {
+        self.tags
+            .iter()
+            .find(|tag| tag.key == name)
+            .map(|found| &found.value)
+    }
+
+    fn sanitize_pathname(name: &str) -> Cow<str> {
+        if name.contains(is_separator) {
+            Cow::Owned(name.replace(is_separator, "_"))
+        } else {
+            Cow::Borrowed(name)
+        }
+    }
+
+    /// Return the output pathname for a track.
+    pub fn pathname(&self) -> PathBuf {
+        let mut buf = PathBuf::new();
+        if let Some(Value::String(artist)) = self.tag_value("ALBUMARTIST") {
+            buf.push(Self::sanitize_pathname(artist).as_ref());
+        } else if let Some(Value::String(artist)) = self.tag_value("ARTIST") {
+            buf.push(Self::sanitize_pathname(artist).as_ref());
+        } else {
+            buf.push("Unknown Artist");
+        }
+
+        if let Some(Value::String(album)) = self.tag_value("ALBUM") {
+            if let Some(Value::String(year)) = self.tag_value("DATE") {
+                buf.push(format!("{} - {}", year, Self::sanitize_pathname(album)));
+            } else {
+                buf.push(Self::sanitize_pathname(album).as_ref());
+            }
+        } else {
+            buf.push("Unknown Album");
+        }
+
+        if let (Some(Value::String(track)), Some(Value::String(title))) =
+            (self.tag_value("TRACKNUMBER"), self.tag_value("TITLE"))
+        {
+            if let Ok(trackno) = <usize as FromStr>::from_str(track) {
+                buf.push(format!(
+                    "{:02}.{}.flac",
+                    trackno,
+                    Self::sanitize_pathname(title)
+                ));
+            } else {
+                buf.push(format!("99.{}.flac", Self::sanitize_pathname(title)));
+            }
+        }
+        buf
+    }
+
+    /// Write a track's
+    /// [STREAM](https://xiph.org/flac/format.html#stream) metadata
+    /// blocks - first STREAMINFO, then the remainder containing
+    /// pictures and vorbis comments.
+    #[instrument(skip(self, to), fields(number = self.number, path = ?self.pathname()), err)]
+    pub fn write_metadata<S: Write>(&self, mut to: S) -> anyhow::Result<()> {
+        to.write_all(b"fLaC")?;
+        let comment = VorbisComment {
+            vendor_string: "asf's silly track splitter".to_string(),
+            comments: self
+                .tags
+                .iter()
+                .map(|tag| (tag.key.to_string(), vec![tag.value.to_string()]))
+                .collect(),
+        };
+        let pictures: Vec<Block> = self
+            .visuals
+            .iter()
+            .map(|visual| {
+                Block::Picture(Picture {
+                    picture_type: PictureType::Other,
+                    mime_type: visual.media_type.to_string(),
+                    description: "".to_string(),
+                    width: visual.dimensions.map(|s| s.width).unwrap_or(0),
+                    height: visual.dimensions.map(|s| s.height).unwrap_or(0),
+                    depth: visual.bits_per_pixel.map(NonZeroU32::get).unwrap_or(0),
+                    num_colors: match visual.color_mode {
+                        Some(symphonia_core::meta::ColorMode::Discrete) => 0,
+                        Some(symphonia_core::meta::ColorMode::Indexed(n)) => n.get(),
+                        None => 0,
+                    },
+                    data: visual.data.to_vec(),
+                })
+            })
+            .collect();
+        let headers = vec![
+            Block::StreamInfo(self.streaminfo.clone()),
+            Block::VorbisComment(comment),
+        ];
+        let mut blocks = headers.into_iter().chain(pictures.into_iter()).peekable();
+        while let Some(block) = blocks.next() {
+            let is_last = blocks.peek().is_none();
+            block.write_to(is_last, &mut to)?;
+        }
+        Ok(())
+    }
+
+    /// Write a STREAM's
+    /// [FRAME](https://xiph.org/flac/format.html#frame) sequence,
+    /// containing compressed audio samples.
+    #[instrument(skip(self, from, to), fields(number = self.number, path = ?self.pathname()), err)]
+    pub fn write_audio<S: Write>(&self, from: &mut FlacReader, mut to: S) -> anyhow::Result<()> {
+        // TODO: Seek to the track start. Currently, this is only called in sequence, so no need to do that.
+        let mut last_end: u64 = 0;
+        let mut frame = OffsetFrame::default();
+        loop {
+            let packet = from
+                .next_packet()
+                .with_context(|| format!("last end: {:?} vs {:?}", last_end, self.end_ts))?;
+
+            let ts = packet.ts;
+            let dur = packet.dur;
+            ma::assert_ge!(
+                ts,
+                self.start_ts,
+                "Packet timestamp is not >= this track's start ts. Potential bug exposed by the previous track.",
+            );
+
+            // Adjust the frame header:
+            // * Adjust sample/frame number such that each track starts at frame/sample 0. This should fix seeking.
+            // * Recompute the 8-bit header CRC
+            // * Recompute the 16-bit footer CRC
+
+            let (updated_buf, _header_matches, _footer_matches) = frame
+                .process(packet)
+                .with_context(|| format!("processing frame at ts {}", ts))?;
+            to.write_all(&updated_buf)?;
+
+            last_end = ts + dur;
+            if last_end >= self.end_ts {
+                return Ok(());
+            }
+        }
+    }
+}
+
+/// A FLAC stream's [Frame](https://xiph.org/flac/format.html#frame),
+/// with samples that are offset such that the first frame has a
+/// frame/sample offset of 0 and the others follow suit.
+///
+/// This is meant to be created once per [Track], and then updated for
+/// all the frames making up that track.
 #[derive(Default)]
 pub struct OffsetFrame {
     initial_offset: Option<u64>,
